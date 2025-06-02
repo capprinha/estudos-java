81b8fa2b0a0d4dd732f7399f2f88254d605c7d61
capprinha <gutoklein21@gmail.com>
2025-06-02 16:45:45 -0300

    Add files via upload

---
+++ ScreeMatch/screenmatch/calculos/CalculadoraDeTempo.java
@@ -0,0 +1,16 @@
+package screenmatch.calculos;
+
+import screenmatch.modelos.Titulo;;
+
+public class CalculadoraDeTempo {
+    
+    private int tempoTotal;
+
+    public int getTempoTotal(){
+        return tempoTotal;
+    }
+
+    public void inclui(Titulo titulo){
+        tempoTotal += titulo.getDuracaoEmMinutos();
+    }
+}
---
+++ ScreeMatch/screenmatch/calculos/Recomendacao.java
@@ -0,0 +1,17 @@
+package screenmatch.calculos;
+
+import screenmatch.modelos.Titulo;
+
+public class Recomendacao {
+    private String recomendacao;
+
+    public void filtra(Classificavel classificavel){
+        if (classificavel.getClassificacao() >= 4) {
+            System.out.println("Esta entre ps preferidos no momento");
+        }   else if(classificavel.getClassificacao() >= 2){
+            System.out.println("Muito bem avaliado no momento");
+        } else {
+            System.out.println("Coloca na sua lista para assistir depois");
+        }
+    }
+}
